35c069aee6fc3c0b96f57016af7915d4206ffcaa
Tharunda De Silva <tharundadesilva@gmail.com>
2025-07-18 23:50:59 +0530

    Attempt model created

--- src/main/java/com/quizWeb/demo/Model/Attempt.java
+++ src/main/java/com/quizWeb/demo/Model/Attempt.java
@@ -1,4 +1,34 @@
 package com.quizWeb.demo.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
 public class Attempt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long attemptId;
+
+    private int marks;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
 }
