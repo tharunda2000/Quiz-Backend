d3893858d2121e68b6df905cc3163c543e734ed9
Tharunda De Silva <tharundadesilva@gmail.com>
2025-07-19 00:07:11 +0530

    Quiz model created

--- src/main/java/com/quizWeb/demo/Model/Quiz.java
+++ src/main/java/com/quizWeb/demo/Model/Quiz.java
@@ -1,4 +1,32 @@
 package com.quizWeb.demo.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long quizId;
+
+    private String quizName;
+    private int duration;
+    private String accessKey;
+
+    @OneToMany(mappedBy = "quiz",cascade = CascadeType.ALL)
+    private List<Attempt> attempts;
+
+    @OneToMany(mappedBy = "quiz",cascade = CascadeType.ALL)
+    private List<Question> questions;
+
 }
