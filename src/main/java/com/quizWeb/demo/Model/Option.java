930bd7e953a268b3a169d6d5e072bf4d5edef138
Tharunda De Silva <tharundadesilva@gmail.com>
2025-07-19 00:00:35 +0530

    Option model created

--- src/main/java/com/quizWeb/demo/Model/Option.java
+++ src/main/java/com/quizWeb/demo/Model/Option.java
@@ -1,4 +1,29 @@
 package com.quizWeb.demo.Model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
 public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long optionId;
+
+    private String option;
+    private Boolean isCorrect;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
 }
