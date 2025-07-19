49af3159543c59fb38527bb8e0b7d1006c53658f
Tharunda De Silva <tharundadesilva@gmail.com>
2025-07-19 19:59:25 +0530

    User service created

--- src/main/java/com/quizWeb/demo/Repository/UserRepository.java
+++ src/main/java/com/quizWeb/demo/Repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.quizWeb.demo.Repository;
 
-public class UserRepository {
+import com.quizWeb.demo.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+
 }
--- src/main/java/com/quizWeb/demo/Service/UserService.java
+++ src/main/java/com/quizWeb/demo/Service/UserService.java
@@ -1,24 +1,41 @@
 package com.quizWeb.demo.Service;
 
 import com.quizWeb.demo.Model.User;
+import com.quizWeb.demo.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
+
+    UserRepository userRepo;
+
     public List<User> getallUsers() {
+        return userRepo.findAll();
     }
 
     public User getUserById(int id) {
+        return userRepo.findById(id).get();
     }
 
     public void addUser(User user) {
+        userRepo.save(user);
     }
 
     public void deleteUser(int id) {
+        userRepo.deleteById(id);
     }
 
-    public void editUser(User user) {
+    public void editUser(User user, int id) {
+        User existingUser = userRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("User does not found"));
+
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+
+        userRepo.save(existingUser);
+
     }
 }
