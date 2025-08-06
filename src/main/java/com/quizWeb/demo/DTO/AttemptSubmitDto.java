d5ebfbf1518574b0ba343b41735540f721cdd436
Tharunda De Silva <tharundadesilva@gmail.com>
2025-08-07 00:07:45 +0530

    Attempt Submit Dto Created

--- src/main/java/com/quizWeb/demo/DTO/AttemptSubmitDto.java
+++ src/main/java/com/quizWeb/demo/DTO/AttemptSubmitDto.java
@@ -1,4 +1,14 @@
 package com.quizWeb.demo.DTO;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class AttemptSubmitDto {
+
+    private int marks;
+    private LocalDateTime endTime;
+    private int userId;
+    private Long quizId;
 }
